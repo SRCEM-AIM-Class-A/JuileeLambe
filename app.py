a6bf40bd2e2d8ed0341c615f2edffed6b185f6a0
Juilee <juileelambe08@gmail.com>
2025-02-19 22:05:28 +0530

    bootstrap styling added to flask app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-     return "<p>Hello, World this flask</p>"
+def home():
+    return render_template("index.html")
 
-if __name__  == "__main__":
+@app.route("/clicked")
+def clicked():
+    return "<h1>Button Clicked! 🎉</h1>"
+
+if __name__ == "__main__":
     app.run(debug=True)
